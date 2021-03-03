adbdf083e9212d5819b3cef87fb1eda5c5e44fa8
Mordegaard <bodme12@gmail.com>
2021-03-03 19:11:51 +0200

    dark theme

--- code.js
+++ code.js
@@ -1,7 +1,6 @@
 window.onload = function() {
-  var canv = id("canvas");
-  var ctx = canv.getContext('2d');
-  ctx.strokeStyle = "black";
+  var canv = id("canvas"), preview = id("nextFigure");
+  var ctx = canv.getContext('2d'), prv = preview.getContext('2d');
   var w = canv.width, h = canv.height;
   const tetris = {
     width: 10,
@@ -11,6 +10,7 @@ window.onload = function() {
     data: null,
     speed: 333,
     originalSpeed: 333,
+    score: 0,
     blocks: [
       [[1,1,0],[0,1,1]],
       [[0,1,1],[1,1,0]],
@@ -28,10 +28,12 @@ window.onload = function() {
       data: null,
     },
     selectedBlock: null,
+    nextBlock: null,
     colors: [
-      "red", "orange", "blue", "green", "purple", "blueviolet", "chartreuse"
+      "#e04654", "#f1c421", "#5b81ea", "#52cc52", "#d844d8", "blueviolet", "chartreuse"
     ]
   };
+  tetris.nextBlock = randomInt(tetris.blocks.length);
 
   function rotateArray(matrix) {
     let result = [];
@@ -99,14 +101,34 @@ window.onload = function() {
     var s = w / tetris.width;
     for (var x=0; x<tetris.width; x++) {
       for (var y=0; y<tetris.height; y++) {
-        if (tetris.data[y][x] == 1) {ctx.fillStyle = tetris.colors[tetris.selectedBlock]; ctx.fillRect(s*x,s*y,s,s);}
-        else if (tetris.data[y][x] == 2) {ctx.fillStyle = "grey"; ctx.fillRect(s*x,s*y,s,s);}
+        ctx.strokeStyle = "rgba(255,255,255,0.2)";
+        if (tetris.data[y][x] == 1) {ctx.strokeStyle = "white"; ctx.fillStyle = tetris.colors[tetris.selectedBlock]; ctx.fillRect(s*x,s*y,s,s);}
+        else if (tetris.data[y][x] == 2) {ctx.strokeStyle = "white"; ctx.fillStyle = "grey"; ctx.fillRect(s*x,s*y,s,s);}
         ctx.strokeRect(s*x,s*y,s,s);
       }
     }
   }
 
   function initFigure() {
+    tetris.speed = tetris.originalSpeed;
+    tetris.selectedBlock = tetris.nextBlock;
+    tetris.nextBlock = randomInt(tetris.blocks.length);
+    tetris.block.data = tetris.blocks[tetris.selectedBlock].slice();
+    tetris.block.w = tetris.block.data[0].length, tetris.block.h = tetris.block.data.length, tetris.block.hw = Math.floor(tetris.block.w/2), tetris.block.hh = Math.floor(tetris.block.h/2);
+    tetris.x = tetris.width / 2; tetris.y = tetris.block.hh;
+    var nX = tetris.blocks[tetris.nextBlock][0].length, nY = tetris.blocks[tetris.nextBlock].length;
+    preview.width = nX * 20; preview.height = nY * 20;
+    prv.clearRect(0,0,preview.width,preview.height);
+    prv.strokeStyle = "white"; prv.fillStyle = tetris.colors[tetris.nextBlock];
+    for (var y=0; y<nY; y++) {
+      for (var x=0; x<nX; x++) {
+        if (tetris.blocks[tetris.nextBlock][y][x]) {
+          prv.fillRect(x*20,y*20,20,20);
+          prv.strokeRect(x*20,y*20,20,20);
+        }
+      }
+    }
+    id("score").innerText = "Ваш счёт: " + tetris.score;
     for (var y=0; y<tetris.height; y++) {
       var row = true;
       for (var x=0; x<tetris.width; x++) {
@@ -114,6 +136,7 @@ window.onload = function() {
         if (!tetris.data[y][x]) row = false;
       }
       if (row) {
+        tetris.score += 100;
         for (var i=0; i<tetris.width; i++) tetris.data[y][i] = 0;
         for (var Y=y-1; Y>0; Y--) {
           for (var X=0; X<tetris.width; X++) {
@@ -124,46 +147,45 @@ window.onload = function() {
         }
       }
     }
-    tetris.speed = tetris.originalSpeed;
-    tetris.selectedBlock = randomInt(tetris.blocks.length);
-    tetris.block.data = tetris.blocks[tetris.selectedBlock].slice();
-    tetris.block.w = tetris.block.data[0].length, tetris.block.h = tetris.block.data.length, tetris.block.hw = Math.floor(tetris.block.w/2), tetris.block.hh = Math.floor(tetris.block.h/2);
-    tetris.x = tetris.width / 2; tetris.y = tetris.block.hh;
     for (var x=0; x<tetris.block.w; x++) {
       for (var y=0; y<tetris.block.h; y++) {
         console.log(tetris.y-tetris.block.hh+y)
         if (tetris.block.data[y][x] == 1) tetris.data[tetris.y-tetris.block.hh+y][tetris.x-tetris.block.w+tetris.block.hw+x] = 1;
       }
-      if (tetris.data[tetris.y+tetris.block.hw][tetris.x-tetris.block.w+tetris.block.hw+x] == 2) {alert("YOU DIED"); location.reload();}
+      if (tetris.data[tetris.y+tetris.block.hw][tetris.x-tetris.block.w+tetris.block.hw+x] == 2) {alert("Вы проиграли. Ваш счёт: " + (tetris.score-10)); location.reload(); return;}
     }
     draw();
     setTimeout(fall, tetris.speed);
   }
 
   function fall() {
-    for (var x=tetris.x-tetris.block.w+tetris.block.hw; x<tetris.x+tetris.block.w-tetris.block.hw; x++) {
-      for (var y=tetris.y-tetris.block.hh; y<tetris.y+tetris.block.h-tetris.block.hh; y++) {
-        if (tetris.data[y][x] == 1) tetris.data[y][x] = 0;
-      }
-    }
-    tetris.y++;
-    for (var x=0; x<tetris.block.w; x++) {
-      for (var y=0; y<tetris.block.h; y++) {
-        if (tetris.block.data[y][x] == 1) tetris.data[tetris.y-tetris.block.hh+y][tetris.x-tetris.block.w+tetris.block.hw+x] = 1;
-      }
-    }
     var block = false;
-    //console.log(tetris.y+tetris.block.hh, tetris.height);
     if (tetris.y+tetris.block.h-tetris.block.hh<tetris.height) {
       for (var y=0; y<tetris.block.h; y++) {
         for (var x=0; x<tetris.block.w; x++) {
           if (tetris.data[tetris.y-tetris.block.hh+y+1][tetris.x-tetris.block.w+tetris.block.hw+x] == 2 && tetris.block.data[y][x]) block = true;
         }
       }
-      if (!block) {draw(); setTimeout(fall, tetris.speed); return;}
+      if (!block) {
+        for (var x=tetris.x-tetris.block.w+tetris.block.hw; x<tetris.x+tetris.block.w-tetris.block.hw; x++) {
+          for (var y=tetris.y-tetris.block.hh; y<tetris.y+tetris.block.h-tetris.block.hh; y++) {
+            if (tetris.data[y][x] == 1) tetris.data[y][x] = 0;
+          }
+        }
+        tetris.y++;
+        for (var x=0; x<tetris.block.w; x++) {
+          for (var y=0; y<tetris.block.h; y++) {
+            if (tetris.block.data[y][x] == 1) tetris.data[tetris.y-tetris.block.hh+y][tetris.x-tetris.block.w+tetris.block.hw+x] = 1;
+          }
+        }
+        draw(); setTimeout(fall, tetris.speed);
+         return;
+      }
     }
+    tetris.score += 10;
     draw();
-    setTimeout(initFigure, tetris.speed);
+    initFigure();
+    //setTimeout(initFigure, tetris.speed);
   }
 
   initFigure();

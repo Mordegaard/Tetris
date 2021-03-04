3683282510cf6ee90fd35a5e80ec48f1fcaddeb2
Mordegaard <bodme12@gmail.com>
2021-03-04 20:55:13 +0200

    textures

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 window.onload = function() {
+  document.body.focus();
   var canv = id("canvas"), preview = id("nextFigure");
   var ctx = canv.getContext('2d'), prv = preview.getContext('2d');
   var w = canv.width, h = canv.height;
+  var bgp_w = null, bgp_h = null;
   const tetris = {
     width: 10,
     height: 20,
@@ -80,9 +82,19 @@ window.onload = function() {
       }
     }
     if (e.key == "ArrowUp") {
-      tetris.block.data = rotateArray(tetris.block.data);
-      tetris.block.w = tetris.block.data[0].length, tetris.block.h = tetris.block.data.length, tetris.block.hw = Math.floor(tetris.block.w/2), tetris.block.hh = Math.floor(tetris.block.h/2);
-      updateFrame();
+      var rotate = true;
+      var block = rotateArray(tetris.block.data);
+      var block_h = block.length, block_w = block[0].length;
+      for (var y=0; y<block_h; y++) {
+        for (var x=0; x <block_w; x++) {
+          if (block[y][x] && (tetris.data[tetris.y-block_h+Math.floor(block_h/2)+y][tetris.x-block_w+Math.floor(block_w/2)+x] == 2 || tetris.data[tetris.y-block_h+Math.floor(block_h/2)+y][tetris.x-block_w+Math.floor(block_w/2)+x] == undefined)) rotate = false;
+        }
+      }
+      if (rotate) {
+        tetris.block.data = block;
+        tetris.block.w = block_w, tetris.block.h = block_h, tetris.block.hw = Math.floor(tetris.block.w/2), tetris.block.hh = Math.floor(tetris.block.h/2);
+        updateFrame();
+      }
     }
     if (e.key == "ArrowDown") {
       tetris.speed = tetris.originalSpeed / 10;
@@ -95,6 +107,9 @@ window.onload = function() {
   });
   tetris.data = new Array(tetris.height);
   for (var i=0; i<tetris.height; i++) tetris.data[i] = new Array(tetris.width);
+  for (var y=0; y<tetris.height; y++) {
+    for (var x=0; x<tetris.width; x++) tetris.data[y][x] = 0;
+  }
 
   function draw() {
     ctx.clearRect(0,0,w,h);
@@ -102,8 +117,16 @@ window.onload = function() {
     for (var x=0; x<tetris.width; x++) {
       for (var y=0; y<tetris.height; y++) {
         ctx.strokeStyle = "rgba(255,255,255,0.2)";
-        if (tetris.data[y][x] == 1) {ctx.strokeStyle = "white"; ctx.fillStyle = tetris.colors[tetris.selectedBlock]; ctx.fillRect(s*x,s*y,s,s);}
-        else if (tetris.data[y][x] == 2) {ctx.strokeStyle = "white"; ctx.fillStyle = "grey"; ctx.fillRect(s*x,s*y,s,s);}
+        if (tetris.data[y][x]) {
+          ctx.strokeStyle = "white";
+          if (tetris.data[y][x] == 1) {ctx.fillStyle = tetris.colors[tetris.selectedBlock]; ctx.fillRect(s*x,s*y,s,s);}
+          else if (tetris.data[y][x] == 2) {
+            ctx.fillStyle = "grey";
+            //ctx.drawImage(bg, s*x*bgp_w, s*y*bgp_h, s*bgp_w, s*bgp_w, s*x, s*y, s, s);
+            ctx.fillRect(s*x,s*y,s,s);
+          }
+          ctx.drawImage(texture, s*x, s*y, s, s);
+        }
         ctx.strokeRect(s*x,s*y,s,s);
       }
     }
@@ -149,7 +172,6 @@ window.onload = function() {
     }
     for (var x=0; x<tetris.block.w; x++) {
       for (var y=0; y<tetris.block.h; y++) {
-        console.log(tetris.y-tetris.block.hh+y)
         if (tetris.block.data[y][x] == 1) tetris.data[tetris.y-tetris.block.hh+y][tetris.x-tetris.block.w+tetris.block.hw+x] = 1;
       }
       if (tetris.data[tetris.y+tetris.block.hw][tetris.x-tetris.block.w+tetris.block.hw+x] == 2) {alert("Вы проиграли. Ваш счёт: " + (tetris.score-10)); location.reload(); return;}
@@ -188,5 +210,12 @@ window.onload = function() {
     //setTimeout(initFigure, tetris.speed);
   }
 
-  initFigure();
+  const texture =  new Image();
+  const bg = new Image();
+  texture.src = "texture.png";
+  bg.src = "bg.png";
+  texture.onload = initFigure;
+  bg.onload = function() {bgp_h = bg.height / h; bgp_w = bg.width / w;}
+
+  //initFigure();
 }
